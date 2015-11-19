669f41863b58d40df7b358e7610fd5db4d6f45c4
bmurr <bmurr@users.noreply.github.com>
2015-11-19 13:24:49 +0000

    Removed several functions from global scope. Added up/down listener.

--- event-page.js
+++ event-page.js
@@ -1,37 +1,40 @@
-var HistoryPanel = function () {
-    var self = this;
+(function () {
+    var HistoryPanel = function () {
+        var self = this;
 
-    self.focus = function () {
-        chrome.windows.update(self.window.id, { "focused": true });
-    };
-
-    self.openPanel = function () {
-        var windowOptions = {
-            'width': 900,
-            'height': 600,
-            'type': 'panel',
-            'url': 'historyPanel.html'
+        self.focus = function () {
+            chrome.windows.update(self.window.id, { "focused": true });
         };
-        chrome.windows.create(windowOptions, function (window) {
-            self.window = window;
-        })
+
+        self.openPanel = function () {
+            var windowOptions = {
+                'width': 900,
+                'height': 600,
+                'type': 'panel',
+                'url': 'historyPanel.html'
+            };
+            chrome.windows.create(windowOptions, function (window) {
+                self.window = window;
+            })
+        };
+
+        self.openPanel();
     };
 
-    self.openPanel();
-};
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request && request.action === 'openHistoryPanel') {
-        if (!!window.historyPanel) {
-            window.historyPanel.focus()
-        } else {
-            window.historyPanel = new HistoryPanel();
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request && request.action === 'openHistoryPanel') {
+            if (!!window.historyPanel) {
+                window.historyPanel.focus()
+            } else {
+                window.historyPanel = new HistoryPanel();
+            }
         }
-    }
-});
+    });
 
-chrome.windows.onRemoved.addListener(function(windowID){
-   if (windowID === window.historyPanel.window.id){
-       window.historyPanel = null;
-   }
-});
+    chrome.windows.onRemoved.addListener(function (windowID) {
+        if (windowID === window.historyPanel.window.id) {
+            window.historyPanel = null;
+        }
+    });
+
+}());
--- history-panel.js
+++ history-panel.js
@@ -1,76 +1,102 @@
-var historyPanelApp = angular.module('historyPanel', []);
+(function () {
 
-historyPanelApp.controller('HistoryPanelCtrl', function ($scope) {
+    var historyPanelApp = angular.module('historyPanel', []);
 
-    $scope.prettyTime = function (date) {
-        return date.getFullYear() + "-"
-            + ("0" + (date.getMonth() + 1)).slice(-2) + "-"
-            + ("0" + date.getDate()).slice(-2) + " "
-            + ("0" + date.getHours()).slice(-2) + ":"
-            + ("0" + date.getMinutes()).slice(-2) + ":"
-            + ("0" + date.getSeconds()).slice(-2) + "."
-            + ("00" + date.getMilliseconds()).slice(-3);
-    };
+    historyPanelApp.controller('HistoryPanelCtrl', function ($scope, historyPanelSettings) {
 
-    $scope.prettyBytes = function (bytes) {
-        if      (bytes>=1000000000) {bytes=(bytes/1000000000).toFixed(2)+' GB';}
-        else if (bytes>=1000000)    {bytes=(bytes/1000000).toFixed(2)+' MB';}
-        else if (bytes>=1000)       {bytes=(bytes/1000).toFixed(2)+' KB';}
-        else if (bytes>1)           {bytes=bytes+' bytes';}
-        else if (bytes==1)          {bytes=bytes+' byte';}
-        else                        {bytes='0 bytes';}
-        return bytes;
-    };
+        $scope.prettyTime = function (date) {
+            return date.getFullYear() + "-"
+                + ("0" + (date.getMonth() + 1)).slice(-2) + "-"
+                + ("0" + date.getDate()).slice(-2) + " "
+                + ("0" + date.getHours()).slice(-2) + ":"
+                + ("0" + date.getMinutes()).slice(-2) + ":"
+                + ("0" + date.getSeconds()).slice(-2) + "."
+                + ("00" + date.getMilliseconds()).slice(-3);
+        };
 
-    $scope.renderTemplate = function() {
-        var usagePromise = new Promise(function (resolve, reject) {
-            chrome.storage.local.getBytesInUse(null, function (bytesInUse) {
-                resolve(bytesInUse);
-            });
-        });
+        $scope.prettyBytes = function (bytes) {
+            if (bytes >= 1000000000) {
+                bytes = (bytes / 1000000000).toFixed(2) + ' GB';
+            }
+            else if (bytes >= 1000000) {
+                bytes = (bytes / 1000000).toFixed(2) + ' MB';
+            }
+            else if (bytes >= 1000) {
+                bytes = (bytes / 1000).toFixed(2) + ' KB';
+            }
+            else if (bytes > 1) {
+                bytes = bytes + ' bytes';
+            }
+            else if (bytes == 1) {
+                bytes = bytes + ' byte';
+            }
+            else {
+                bytes = '0 bytes';
+            }
+            return bytes;
+        };
 
-        var dataPromise = new Promise(function (resolve, reject) {
-            chrome.storage.local.get(null, function (historyObject) {
-                resolve(historyObject);
-            });
-        });
-
-        Promise.all([usagePromise, dataPromise]).then(function (values) {
-            $scope.history = [];
-
-            var logCount = 0;
-            angular.forEach(values[1].history, function(value, key){
-                value.prettyTimestamp = $scope.prettyTime(new Date(value.timestamp));
-                value.location = value.url.indexOf('localhost') !== -1 ? 'localhost' : 'appengine';
-                value.contentLength = value.content.length;
-                $scope.history.push(value);
-//                $scope.options.selectedHistoryItem = $scope.history[Object.keys($scope.history)[0]];
-                logCount += 1;
+        $scope.renderTemplate = function () {
+            var usagePromise = new Promise(function (resolve, reject) {
+                chrome.storage.local.getBytesInUse(null, function (bytesInUse) {
+                    resolve(bytesInUse);
+                });
             });
 
-            $scope.summary = {
-                bytesInUse: $scope.prettyBytes(values[0]),
-                logCount: logCount
-            };
+            var dataPromise = new Promise(function (resolve, reject) {
+                chrome.storage.local.get(null, function (historyObject) {
+                    resolve(historyObject);
+                });
+            });
 
-            $scope.$apply();
-        });
+            Promise.all([usagePromise, dataPromise]).then(function (values) {
 
-    };
+                var logCount = 0;
+                angular.forEach(values[1].history, function (value, key) {
+                    value.prettyTimestamp = $scope.prettyTime(new Date(value.timestamp));
+                    value.location = value.url.indexOf('localhost') !== -1 ? 'localhost' : 'appengine';
+                    value.contentLength = value.content.length;
+                    historyPanelSettings.history.push(value);
+                    $scope.history.push(value);
+//                $scope.settings.selectedHistoryItem = $scope.history[Object.keys($scope.history)[0]];
+                    logCount += 1;
+                });
 
-    $scope.selectHistoryItem = function(item){
-        $scope.options.selectedHistoryItem = item;
-    };
+                $scope.summary = {
+                    bytesInUse: $scope.prettyBytes(values[0]),
+                    logCount: logCount
+                };
 
-    $scope.renderTemplate();
-    $scope.options = {selectedHistoryItem:null};
+                $scope.$apply();
+            });
 
-});
+        };
 
-historyPanelApp.directive('codeArea', function(){
+        $scope.selectHistoryItem = function (item, index) {
+            $scope.settings.selectedHistoryItem = item;
+            $scope.settings.selectedHistoryItemIndex = $scope.settings.history.length - (index + 1);
+        };
 
-    function link(scope, element, attrs) {
-        var codeConfigOptions = {
+        $scope.keyDownHistoryItems = function (event) {
+            console.log('keypress');
+            console.log(event);
+        };
+
+        $scope.renderTemplate();
+        $scope.settings = historyPanelSettings;
+        $scope.history = [];
+
+    });
+
+    historyPanelApp.service('historyPanelSettings', function(){
+        this.selectedHistoryItem = null;
+        this.history = [];
+    });
+
+    historyPanelApp.directive('codeArea', function () {
+
+        function link(scope, element, attrs) {
+            var codeConfigOptions = {
                 "mode": "python",
                 "theme": "default",
                 "indentUnit": 4,
@@ -87,46 +113,102 @@ historyPanelApp.directive('codeArea', function(){
                 "dragDrop": false,
                 "viewportMargin": Infinity
             };
-        scope.codearea = CodeMirror.fromTextArea(element[0], codeConfigOptions);
+            scope.codearea = CodeMirror.fromTextArea(element[0], codeConfigOptions);
 
-        scope.$watch('item', function(value) {
-            scope.codearea.setValue(scope.item.content);
-        });
-
-    }
-
-    return {
-        scope: {
-                item: '=?'
-        },
-        link: link
-    };
-});
-
-historyPanelApp.directive('resizer', function ($document) {
-    return function (scope, element, attr) {
-        var startX = 200, x = 0
-        element.css({
-            border: '1px solid red'
-        });
-        element.on('mousedown', function (event) {
-            // Prevent default dragging of selected content
-            event.preventDefault();
-            startX = event.screenX - x;
-            $document.on('mousemove', mousemove);
-            $document.on('mouseup', mouseup);
-        });
-
-        function mousemove(event) {
-            x = event.screenX - startX;
-            element.css({
-                left: x + 'px'
+            scope.$watch('item', function (value) {
+                scope.codearea.setValue(scope.item.content);
             });
+
         }
 
-        function mouseup() {
-            $document.off('mousemove', mousemove);
-            $document.off('mouseup', mouseup);
+        return {
+            scope: {
+                item: '=?'
+            },
+            link: link
+        };
+    });
+
+    historyPanelApp.directive('resizer', function ($document) {
+        return function (scope, element, attr) {
+            var startX = 200, x = 0
+            element.css({
+                border: '1px solid red'
+            });
+            element.on('mousedown', function (event) {
+                // Prevent default dragging of selected content
+                event.preventDefault();
+                startX = event.screenX - x;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            function mousemove(event) {
+                x = event.screenX - startX;
+                element.css({
+                    left: x + 'px'
+                });
+            }
+
+            function mouseup() {
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+            }
+        };
+    });
+
+    historyPanelApp.directive('scrollUpDown', function ($timeout, historyPanelSettings) {
+
+        function handleUpDownEvent(scope, event){
+            console.log([scope, event]);
+            var selectedRowElement = jQuery('.column-one .selected');
+            var rowHeight = selectedRowElement.height();
+            var selectedRowElementPosition = selectedRowElement.position().top;
+
+            var headerHeight = jQuery('.data-header').height();
+            var footerHeight = jQuery('.summary-bar').height();
+
+            var scrollingElement = jQuery('.column-one');
+            var scrollingElementHeight = scrollingElement.height() - headerHeight - footerHeight;
+            var scrollingElementPosition = scrollingElement.scrollTop();
+            var scrollingElementMaxScrollPosition = scrollingElement.prop('scrollHeight') - scrollingElementHeight;
+            if (event.which == UP_KEY) {
+                if (selectedRowElementPosition - rowHeight < scrollingElementPosition){
+                    scrollingElement.scrollTop(Math.max(scrollingElementPosition - rowHeight, 0));
+                }
+                newIndex = Math.min(historyPanelSettings.history.length - 1, historyPanelSettings.selectedHistoryItemIndex + 1);
+            } else if (event.which == DOWN_KEY) {
+                if (selectedRowElementPosition + (rowHeight * 2) > scrollingElementPosition + scrollingElementHeight){
+                    scrollingElement.scrollTop(Math.min(scrollingElementPosition + rowHeight - ((scrollingElementPosition + rowHeight) % rowHeight) , scrollingElementMaxScrollPosition));
+                }
+                newIndex = Math.max(0, historyPanelSettings.selectedHistoryItemIndex - 1);
+            }
+
+            scope.$parent.$apply(function(){
+                historyPanelSettings.selectedHistoryItem = historyPanelSettings.history[newIndex];
+                historyPanelSettings.selectedHistoryItemIndex = newIndex;
+            });
+
         }
-    };
-});
+
+        var newIndex;
+        var UP_KEY = 38;
+        var DOWN_KEY = 40;
+        var timer;
+
+
+        return function (scope, element, attrs) {
+            element.on("keydown", function (event) {
+                if (event.which == UP_KEY || event.which == DOWN_KEY){
+                    console.log(timer);
+                    $timeout.cancel(timer);
+                    timer = $timeout(handleUpDownEvent, 1, true, scope, event);
+
+                    event.stopPropagation();
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+
+}());
