1a0668da8bd468c872695b19413e5bfd5b2c3f56
bmurr <bmurr@users.noreply.github.com>
2015-11-25 17:32:27 +0000

    Moved the grid into a standalone module. Restructured project.

---
+++ flex-grid/directives/flexGrid.js
@@ -0,0 +1,167 @@
+angular.module('flexGrid')
+    .directive('flexGrid', function ($timeout, $window, $document) {
+        var config, columnWidthPercentages, columnWidthPixels;
+
+        function syncHeaderDataColumnWidths(rootElement) {
+            var headerColumns = rootElement.find('.header-container col');
+            var dataColumns = rootElement.find('.data-container tr:first-of-type td');
+            angular.forEach(dataColumns, function (item, index) {
+                var dataColumnWidth = angular.element(item).outerWidth();
+                angular.element(headerColumns[index]).css('width', dataColumnWidth);
+            })
+
+        }
+
+        function setDataColumnWidths(rootElement) {
+            var columnWidth;
+            var dataColumns = rootElement.find('.data-container col');
+            var rootElementWidth = rootElement.outerWidth();
+
+            angular.forEach(dataColumns, function (item, index) {
+                if (!columnWidthPixels[index]) {
+                    columnWidth = Math.max(rootElementWidth * (columnWidthPercentages[index] / 100.0), config.minWidth);
+                    columnWidthPixels[index] = columnWidth;
+
+                } else {
+                    columnWidth = columnWidthPixels[index];
+                }
+                angular.element(item).css('width', columnWidth + 'px');
+            });
+
+        }
+
+        function setColumnResizerPositions(rootElement) {
+            var dataColumns = rootElement.find('.data-container tr:first-of-type td');
+            var resizers = rootElement.find('.data-grid-resizer');
+            var totalLeft = 0;
+            angular.forEach(dataColumns.slice(0, -1), function (item, index) {
+                totalLeft += angular.element(item).outerWidth();
+                angular.element(resizers[index]).css('left', totalLeft - 2);
+            });
+        }
+
+        function onResize($rootElement) {
+            setDataColumnWidths($rootElement);
+            syncHeaderDataColumnWidths($rootElement);
+            setColumnResizerPositions($rootElement);
+        }
+
+        function resizeColumn($event) {
+
+            function mousemove(event) {
+                var columnWidthPixelsCopy = columnWidthPixels.slice();
+                var x = event.screenX - startX;
+
+                columnWidthPixelsCopy[resizerIndex] = initialColumnWidths[resizerIndex] + x;
+                columnWidthPixelsCopy[columnWidthPixelsCopy.length - 1] = initialColumnWidths[columnWidthPixelsCopy.length - 1] - x;
+
+                if (columnWidthPixelsCopy.every(function (item) {
+                    return item >= config.minWidth;
+                })) {
+                    columnWidthPixels = columnWidthPixelsCopy;
+                    angular.forEach(columnWidthPercentages, function (item, index) {
+                        var pixelAsPercentage = Math.max(columnWidthPixels[index] / rootElement.outerWidth() * 100.0, config.minWidth);
+                        columnWidthPercentages[index] = pixelAsPercentage;
+                    })
+                }
+                onResize(rootElement);
+            }
+
+            function mouseup() {
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+            }
+
+            $event.preventDefault();
+            $event.stopPropagation();
+            var element = angular.element($event.target);
+            var rootElement = element.closest('.flex-grid-root');
+            var dataColumns = rootElement.find('.data-container tr:first-of-type td');
+
+            var startX = event.screenX;
+            var resizerIndex = element.data('index');
+            var initialColumnWidths = columnWidthPixels.slice();
+
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+        }
+
+        function keyDownHandler(event) {
+            var Keys = {
+                Left: { code: 37, name: "\u2190" },
+                Up: { code: 38, name: "\u2191" },
+                Right: { code: 39, name: "\u2192" },
+                Down: { code: 40, name: "\u2193" }
+            };
+
+            function upKeyHandler() {
+
+            }
+
+            function downKeyHandler() {
+
+            }
+
+            if (event.keyCode === Keys.Up.code) {
+                // event.preventDefault();
+                // event.stopPropgation();
+                upKeyHandler();
+            } else if (event.keyCode === Keys.Down.code) {
+                // event.preventDefault();
+                // event.stopPropgation();
+                downKeyHandler();
+            }
+            console.log(event);
+        }
+
+        function postLink($scope, $element, $attr, ctrl, $transcludeFn) {
+
+            config = $scope.config;
+
+            $scope.numColumns = config.numColumns;
+            columnWidthPercentages = config.columnWidthPercentages;
+            columnWidthPixels = [];
+
+            $scope.resizeColumn = resizeColumn;
+            $scope.selectRow = selectRow;
+
+            function selectRow(event, rowIndex, columnIndex) {
+                $scope.$emit('row-selected', {row: rowIndex, column: columnIndex });
+                $scope.config.selectedCallback(event, rowIndex, columnIndex);
+                $scope.selectedRowIndex = rowIndex;
+            }
+
+            angular.element($window).bind('resize', function () {
+                columnWidthPixels = [];
+                onResize($element);
+            });
+
+            $scope.$on('resize', function () {
+                columnWidthPixels = [];
+                onResize($element);
+            });
+
+//            angular.element($element).on('keydown', keyDownHandler);
+
+
+            //$timeout is run after iterpolation/rendering is complete.
+            $timeout(function () {
+                onResize($element);
+            })
+        }
+
+        function compile($element, $attr) {
+            return postLink;
+        }
+
+        return {
+            restrict: 'AE',
+            scope: {
+                config: '='
+            },
+            priority: 1000,
+            terminal: true,
+            compile: compile,
+            templateUrl: 'flex-grid/templates/flexGridTemplate.html'
+        };
+    });
---
+++ history-panel/controllers/historyPanelCtrl.js
@@ -0,0 +1,93 @@
+angular.module('historyPanel')
+    .controller('HistoryPanelCtrl', function ($scope, flexGridConfigFactory) {
+
+        $scope.prettyTime = function (date) {
+            return date.getFullYear() + "-"
+                + ("0" + (date.getMonth() + 1)).slice(-2) + "-"
+                + ("0" + date.getDate()).slice(-2) + " "
+                + ("0" + date.getHours()).slice(-2) + ":"
+                + ("0" + date.getMinutes()).slice(-2) + ":"
+                + ("0" + date.getSeconds()).slice(-2) + "."
+                + ("00" + date.getMilliseconds()).slice(-3);
+        };
+
+        $scope.prettyBytes = function (bytes) {
+            if (bytes >= 1000000000) {
+                bytes = (bytes / 1000000000).toFixed(2) + ' GB';
+            }
+            else if (bytes >= 1000000) {
+                bytes = (bytes / 1000000).toFixed(2) + ' MB';
+            }
+            else if (bytes >= 1000) {
+                bytes = (bytes / 1000).toFixed(2) + ' KB';
+            }
+            else if (bytes > 1) {
+                bytes = bytes + ' bytes';
+            }
+            else if (bytes == 1) {
+                bytes = bytes + ' byte';
+            }
+            else {
+                bytes = '0 bytes';
+            }
+            return bytes;
+        };
+
+        $scope.renderTemplate = function () {
+            var usagePromise = new Promise(function (resolve, reject) {
+                chrome.storage.local.getBytesInUse(null, function (bytesInUse) {
+                    resolve(bytesInUse);
+                });
+            });
+
+            var dataPromise = new Promise(function (resolve, reject) {
+                chrome.storage.local.get(null, function (historyObject) {
+                    resolve(historyObject);
+                });
+            });
+
+            Promise.all([usagePromise, dataPromise]).then(function (values) {
+
+                var logCount = 0;
+                var history = [];
+                angular.forEach(values[1].history, function (value, key) {
+                    value.prettyTimestamp = $scope.prettyTime(new Date(value.timestamp));
+                    value.location = value.url.indexOf('localhost') !== -1 ? 'localhost' : 'appengine';
+                    value.contentLength = value.content.length;
+                    history.push(value);
+                    logCount += 1;
+                });
+                history.reverse();
+
+                //TODO: virtualize list
+                history = history.slice(0, 40);
+
+                $scope.summary = {
+                    bytesInUse: $scope.prettyBytes(values[0]),
+                    logCount: logCount
+                };
+
+                $scope.historyConfig.setData(history);
+                $scope.$apply();
+            });
+
+        };
+
+        function selectedCallback(event, rowIndex, columnIndex){
+            $scope.historyConfig.selectedHistoryItem = $scope.historyConfig.data[rowIndex];
+            var target = event.target;
+            var columnOneWidth = angular.element(angular.element(target).closest('.flex-grid-root').find('.header-container col')[0]).css('width');
+            $scope.columnOneStyle = $scope.historyConfig.selectedHistoryItem ? '0 0 ' + columnOneWidth : '1 1 auto';
+        }
+
+        $scope.history = [];
+        $scope.historyConfig = new flexGridConfigFactory.FlexGridConfig();
+        $scope.historyConfig.setHeaderMap({prettyTimestamp: 'Timestamp', url: 'URL', location: 'Location', contentLength: 'Size', content: 'Content'});
+        $scope.historyConfig.setColumnWidthPercentages([20, 25, 10, 5, 40]);
+        $scope.historyConfig.setSelectedCallback(selectedCallback);
+        $scope.historyConfig.numColumns = [0, 1, 2, 3, 4];
+
+        $scope.renderTemplate();
+
+
+    });
